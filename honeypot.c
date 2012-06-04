e0b4223788190fe551bee3a30ed1c5f6b982fad0
Jason A. Donenfeld <Jason@zx2c4.com>
2012-06-04 17:57:12 +0200

    Add the original license.

--- honeypot.c
+++ honeypot.c
@@ -1,16 +1,42 @@
 /*
  * honeypot.c
  * 
- * Copyright (C) 2012 Jason A. Donenfeld <Jason@zx2c4.com>. All rights reserved.
  * 
  * This is telnet honeypot server. It asks the user for a username and password
  * and logs it shamelessly to a file.
  * 
+ * This honeypot drops all privileges and chroots to /var/empty, after opening the
+ * log file and binding to the telnet port.
+ * 
+ * 
+ * Copyright (C) 2012 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.
+ * 
  * Much of the telnet setup logic has been taken from the hilarious nyancat
  * telnet server, nyancat.c, which is Copyright 2011 by Kevin Lange.
  * 
- * This honeypot drops all privileges and chroots to /var/empty, after opening the
- * log file and binding to the telnet port.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal with the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimers.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimers in the
+ *      documentation and/or other materials provided with the distribution.
+ *   3. Neither the names of the Jason A. Donenfeld, Association for Computing
+ *      Machinery, Kevin Lange, nor the names of its contributors may be used
+ *      to endorse or promote products derived from this Software without
+ *      specific prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * WITH THE SOFTWARE.
  * 
  */
 
